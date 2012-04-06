7b76c175cd159d66f675cb742d0b7c82879a27ba
BUATHIER Quentin <quentin01@live.fr>
2012-04-06 17:01:53 +0200

    Correction du emit du client

--- lib/server/Client.php
+++ lib/server/Client.php
@@ -5,14 +5,18 @@ namespace PHPPush;
 class Client implements \ArrayAccess {
 	protected $id;
 	protected $file;
+	protected $server;
 	
 	protected $lastRecoveryData = 0;
 	protected $data = array();
 	
-	public function __construct($id)
+	public function __construct($id, $server)
 	{
 		$this->id = $id;
 		$this->file = __DIR__ . '/../pipes/clients/' . $this->id;
+		$this->server = $server;
+		
+		$this->data["id"] = $this->id;
 		
 		if(file_exists($this->file))
 			$this->loadData();
@@ -38,8 +42,8 @@ class Client implements \ArrayAccess {
 	public function offsetSet($name, $value)
 	{
 		$this->checkData();
-		$data[$name] = $value;
 		
+		$this->data[$name] = $value;
 		$this->save();
 	}
 	
@@ -63,12 +67,29 @@ class Client implements \ArrayAccess {
 	public function loadData()
 	{
 		$this->lastRecoveryData = time();
-		$this->data = json_decode(file_get_contents($this->file));
+		$this->data = unserialize(file_get_contents($this->file));
 	}
 	
 	public function save()
 	{
-		file_put_contents($this->file, json_encode($this->data));
+		file_put_contents($this->file, serialize($this->data));
 		$this->lastRecoveryData = time();
+	}
+	
+	public function emit($event, $data)
+	{
+		$this->server->addEvent($this['id'], $event, $data);
+	}
+	
+	public function broadcast($event, $data)
+	{
+		foreach($this->server->getClients() as $client)
+		{
+			if($client['id'] != $this['id'])
+				$this->server->addEvent($client['id'], $event, $data);
+		}
+		
+		if($this->server->currentClient['id'] != $this['id'])
+			$this->server->addEvent($this->server->currentClient['id'], $event, $data);
 	}
 }
--- lib/server/PHPPush.php
+++ lib/server/PHPPush.php
@@ -4,12 +4,13 @@ require 'Client.php';
 use \PHPPush\Client as Client;
 
 class PHPPush {
-	protected $events = array();
 	protected $clients = array();
 	
 	public $currentClient = null;
 	
 	public function __construct() {
+		header('Content-Type : text/plain; charset=UTF-8');
+		
 		if(!isset($_SESSION['phppush']['id']))
 		{
 			do {
@@ -19,7 +20,19 @@ class PHPPush {
 			$_SESSION['phppush']['id'] = $id;
 		}
 	
-		$this->currentClient = new Client($_SESSION['phppush']['id']);
+		$this->currentClient = new Client($_SESSION['phppush']['id'], $this);
+		$this->currentClient['lastConnection'] = time();
+		
+		$dirClients = opendir(__DIR__ . '/../pipes/clients/'); 
+
+		while($file = readdir($dirClients)) {
+			if(substr($file, 0, 1) != '.' && !is_dir($dirClients.$file) && $file != $_SESSION['phppush']['id'])
+			{
+				$this->clients[$file] = new Client($file, $this);
+			}
+		}
+
+		closedir($dirClients);
 	}
 	
 	protected function clientExists($id)
@@ -27,29 +40,75 @@ class PHPPush {
 		return file_exists(__DIR__ . '/../pipes/clients/' . $id);
 	}
 	
-	public function on($name, $callback)
+	public function addEvent($id, $event, $data)
 	{
-		$this->events[$name] = $callback;
+		$messagesDir = __DIR__ . '/../pipes/messages/';
+		$messagesFile = $messagesDir . $id;
+		
+		$dataEvent = array();
+			
+		if(file_exists($messagesFile))
+		{
+			$dataEvent = json_decode(file_get_contents($messagesFile));
+		}
+			
+		$dataEvent[] = array('event' => $event, 'data' => json_encode($data));
+		file_put_contents($messagesFile, json_encode($dataEvent));
 	}
 	
-	public function lauch()
+	public function emit($event, $data)
 	{
+		$messagesDir = __DIR__ . '/../pipes/messages/';
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		$this->broadCast($event, $data);
+		$this->addEvent($this->currentClient['id'], $event, $data);
+	}
+	
+	public function broadcast($event, $data)
+	{
+		$messagesDir = __DIR__ . '/../pipes/messages/';
 		
 		foreach($this->clients as $client)
 		{
-			$client->save();
+			$this->addEvent($client['id'], $event, $data);
 		}
+	}
+	
+	public function launch()
+	{
+		$messagesDir = __DIR__ . '/../pipes/messages/';
+		$messagesFile = $messagesDir . $this->currentClient["id"];
 		
-		exit();
+		if(isset($_POST['event']))
+		{
+			$event = $_POST['event'];
+			$data = json_decode($_POST['data']);
+			
+			$methodName = 'on' . ucfirst($event);
+			
+			if(method_exists($this, $methodName))
+			{
+				$this->$methodName($data);
+			}
+			
+			exit();
+		}
+		elseif(isset($_POST['get']))
+		{
+			if(file_exists($messagesFile))
+			{
+				echo file_get_contents($messagesFile);
+				unlink($messagesFile);
+			}
+			else
+			{
+				echo json_encode(array());
+			}
+		}
+	}
+	
+	public function getClients()
+	{
+		return $this->clients;
 	}
 }
