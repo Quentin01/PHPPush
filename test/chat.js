e5a96a190494072489a6269bd54d8368854837dd
BUATHIER Quentin <quentin01@live.fr>
2012-04-06 18:05:53 +0200

    Mise en ligne de la démonstration de chat

---
+++ test/chat.js
@@ -0,0 +1,240 @@
+/* Initialisation */
+
+// Le pseudo
+
+var nick = 'Anonyme';
+
+// Lorsque la page est chargé on se connecte / quand l'utilisateur quitte la page on se déconnecte
+
+window.onload = function() {
+	afficherConnexion();
+	
+	PHPPush.connect('server.php', 2000);
+	PHPPush.launch();
+};
+
+/* Reception d'evenements */
+
+// La connexion à échoué
+PHPPush.on('connexionFailed', function(message) {
+	addErrorMessage("Connexion failed : " + message);
+});
+			
+// La connexion a réussit
+PHPPush.on('connexionSucces', function() {
+	afficherDeconnexion();
+	addUser(nick);
+	addAgreeMessage("Vous vous êtes bien connecté avec le pseudo " + nick);
+});		
+
+// On a recu un message
+PHPPush.on('message', function(data) {
+	addUserMessage(data.nick, data.message);
+});
+
+// Quelqu'un vient de se connecter	
+PHPPush.on('connexion', function(nick) {
+	addAgreeMessage(nick + " vient de se connecter !");
+	addUser(nick);
+});
+
+// Quelqu'un vient de se déconnecter		
+PHPPush.on('deconnexion', function(nick) {
+	addErrorMessage(nick + " vient de se déconnecter !");
+	removeUser(nick);
+});
+
+// On a reçu la liste des membres connectés actuellement		
+PHPPush.on('memberList', function(users) {
+	for (var i = 0; i < users.length; i++) {
+		addUser(users[i]);
+	}
+});
+
+/* Différentes fonctions appelées suite à des événements */
+
+function connexion(nick) {
+	PHPPush.emit('connexion', nick);
+}
+			
+function deconnexion() {
+	PHPPush.emit('deconnexion');
+	removeUser(nick);
+}
+			
+function sendMessage(message) {
+	PHPPush.emit('message', message);
+}
+
+/* Fonction d'affichage des différents éléments */
+
+function afficherConnexion() {
+	var top = document.getElementById('top');
+	top.innerHTML = '';
+				
+	var text = document.createTextNode("Nick : ");
+	top.appendChild(text);
+				
+	var inputNick = document.createElement('input');
+	inputNick.value = nick;
+	inputNick.type = "text";
+	top.appendChild(inputNick);
+				
+	var text = document.createTextNode(" - ");
+	top.appendChild(text);
+				
+	var linkConnexion = document.createElement('a');
+	linkConnexion.href = "#";
+	linkConnexion.appendChild(document.createTextNode("Connexion"));
+	top.appendChild(linkConnexion);
+				
+	hideBottom();
+				
+	linkConnexion.onclick = function() {
+		nick = inputNick.value;
+		connexion(nick);
+	};
+}
+
+function showBottom() {
+	hideBottom();
+				
+	var bottom = document.getElementById('bottom');
+				
+	var inputMessage = document.createElement('input');
+	inputMessage.placeholder = "Votre message ...";
+	inputMessage.type = "text";
+	inputMessage.id = "message";
+	inputMessage.style.width = "84%";
+	inputMessage.autocomplete = "off";
+				
+	bottom.appendChild(inputMessage);
+				
+	inputMessage.onkeypress = function(e) {
+		var keyCode = e.keyCode;
+		
+		if(keyCode == 13)
+		{
+			var message = inputMessage.value;
+			inputMessage.value = "";
+	
+			sendMessage(message);
+		}
+	};
+}
+
+function hideBottom() {
+	var bottom = document.getElementById('bottom');
+	bottom.innerHTML = "";
+}
+
+function afficherDeconnexion() {
+	var top = document.getElementById('top');
+	top.innerHTML = '';
+				
+	var text = document.createTextNode("Bienvenue ");
+	top.appendChild(text);
+				
+	var strongNick = document.createElement('strong');
+	strongNick.appendChild(document.createTextNode(nick));
+	top.appendChild(strongNick);
+				
+	var text = document.createTextNode(" - ");
+	top.appendChild(text);
+				
+	var linkDeconnexion = document.createElement('a');
+	linkDeconnexion.href = "#";
+	linkDeconnexion.appendChild(document.createTextNode("Deconnexion"));
+	top.appendChild(linkDeconnexion);
+				
+	showBottom()
+				
+	linkDeconnexion.onclick = function() {
+		deconnexion();
+		addErrorMessage("Vous venez de vous déconnecter !");
+		afficherConnexion();
+	};
+}
+
+function scroolBottom()
+{
+	document.getElementById('messages-box').scrollTop = document.getElementById('messages-box').scrollHeight; 
+}
+
+function addUserMessage(nick, message)
+{
+	var messagesBox = document.getElementById('messages-box');
+				
+	var divMessage = document.createElement('div');
+	divMessage.className = "userMessage";
+				
+	var strongNick = document.createElement('strong');
+	strongNick.appendChild(document.createTextNode(nick));
+	divMessage.appendChild(strongNick);
+				
+	var text = document.createTextNode(" : " + message);
+	divMessage.appendChild(text);
+				
+	messagesBox.appendChild(divMessage);
+	scroolBottom();
+}
+			
+function addErrorMessage(message)
+{
+	var messagesBox = document.getElementById('messages-box');
+				
+	var divMessage = document.createElement('div');
+	divMessage.className = "errorMessage";
+				
+	var text = document.createTextNode(message);
+	divMessage.appendChild(text);
+				
+	messagesBox.appendChild(divMessage);
+	scroolBottom();
+}
+			
+function addAgreeMessage(message)
+{
+	var messagesBox = document.getElementById('messages-box');
+				
+	var divMessage = document.createElement('div');
+	divMessage.className = "agreeMessage";
+				
+	var text = document.createTextNode(message);
+	divMessage.appendChild(text);
+				
+	messagesBox.appendChild(divMessage);
+	scroolBottom();
+}
+			
+function addUser(nick)
+{
+	var usersBox = document.getElementById('users-box');
+				
+	var divUser = document.createElement('div');
+	divUser.className = "user";
+				
+	var text = document.createTextNode(nick);
+	divUser.appendChild(text);
+				
+	usersBox.appendChild(divUser);
+}
+			
+function removeUser(nick)
+{
+	var usersBox = document.getElementById('users-box');
+	var children  = usersBox.childNodes;
+				
+	var text = '';
+
+	for (var i = 0, c = children.length; i < c; i++) {
+		text = children[i].textContent || children[i].innerText || '';
+			
+		if(text == nick)
+		{
+			usersBox.removeChild(children[i]);
+			return;
+		}
+	}
+}
+
--- test/server.php
+++ test/server.php
@@ -2,4 +2,96 @@
 session_start();
 require "../lib/server/PHPPush.php";
 
-$phppush = new PHPPush();
+class ChatServer extends PHPPush {
+	protected $nicks = array();
+	
+	// On récupére les personnes connectés
+	protected function getNicks()
+	{
+		if(file_exists('nicks'))
+		{
+			$this->nicks = array_unique(array_merge(unserialize(file_get_contents('nicks')), $this->nicks));
+		}
+	}
+	
+	// On sauvegarde les personnes connectés en prenant bien garde à ce que le fichier n'est pas déjà été modifié
+	protected function saveNicks()
+	{
+		$this->getNicks();
+		file_put_contents('nicks', serialize($this->nicks));
+	}
+	
+	// On supprime une personne connecté
+	protected function delNick($nick)
+	{
+		$this->getNicks();
+		unset($this->nicks[array_search($nick, $this->nicks)]);
+		file_put_contents('nicks', serialize($this->nicks));
+	}
+	
+	// L'utilisateur essaye de se connecter
+	protected function onConnexion($nick)
+	{
+		$this->getNicks();
+		$this->currentClient->emit('memberList', $this->nicks);
+		
+		if(array_search($nick, $this->nicks) !== false)
+		{
+			$this->currentClient->emit('connexionFailed', 'Nick already in use');
+			return;
+		}
+		
+		$this->nicks[] = $nick;
+		$this->saveNicks();
+		
+		$this->currentClient['nick'] = $nick;
+		
+		$this->currentClient->emit('connexionSucces');
+		$this->broadcast('connexion', $nick);
+	}
+	
+	// L'utilisateur nous envoit un message
+	protected function onMessage($message)
+	{
+		if(isset($this->currentClient['nick']))
+		{
+			if(empty($message))
+				return;
+				
+			$this->emit('message', array(
+				'nick' => $this->currentClient['nick'],
+				'message' => $message
+			));
+		}
+		else
+		{
+			$this->currentClient->emit('forbid', 'You can\'t send a message without connexion');
+		}
+	}
+	
+	// L'utilisateur vient de se déconnecter
+	protected function onDeconnexion($data)
+	{
+		if(isset($this->currentClient['nick']))
+		{
+			$this->delNick($this->currentClient['nick']);
+			$this->broadcast('deconnexion', $this->currentClient['nick']);
+			
+			unset($this->currentClient['nick']);
+		}
+	}
+	
+	// Un utilisateur fait un timeout
+	protected function onDisconnect($client)
+	{
+		if(isset($client['nick']))
+		{
+			$this->delNick($client['nick']);
+			$this->broadcast('deconnexion', $client['nick']);
+			
+			unset($client['nick']);
+		}
+	}
+}
+$server = new ChatServer();
+$server->launch();
